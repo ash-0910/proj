9f4f7044276fcf879a00b049b89feb79a4934b10
ashish <ashishsurti0910@gmail.com>
2021-12-01 01:18:47 -0500

    created a login and main page UI

---
+++ app/src/main/java/com/ashishsurti/finalproject/CoursesPage.java
@@ -0,0 +1,14 @@
+package com.ashishsurti.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoursesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_page);
+    }
+}
